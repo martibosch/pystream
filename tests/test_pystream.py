74af71eb6e748912143a508df6b0d89db9f704ad
Martí Bosch <marti.bosch@epfl.ch>
2019-02-01 15:25:35 +0100

    monthl simulation (with heat as an external argument)

--- tests/test_pystream.py
+++ tests/test_pystream.py
@@ -2,6 +2,7 @@ import unittest
 
 import rasterio
 import richdem
+import xarray as xr
 
 import pystream as pst
 
@@ -14,8 +15,9 @@ class TestMonthlySimulation(unittest.TestCase):
         self.prec_fp = 'tests/input_data/prec.nc'
         self.temp_fp = 'tests/input_data/temp.nc'
         self.res = (.05, .05)
+        self.num_months = 24
 
-    def test_io(self):
+    def test_io_terrain(self):
         with rasterio.open(self.dem_fp) as dem_src:
             dem_arr = dem_src.read(1)
         with rasterio.open(self.cropf_fp) as cropf_src:
@@ -43,49 +45,86 @@ class TestMonthlySimulation(unittest.TestCase):
                                   self.prec_fp, self.temp_fp).res, self.res)
         self.assertEqual(
             pst.MonthlySimulation(dem_arr, self.cropf_fp, whc_arr,
-                                  self.prec_ds, self.temp_ds).res, self.res)
+                                  self.prec_fp, self.temp_fp).res, self.res)
         self.assertEqual(
             pst.MonthlySimulation(
                 dem_arr,
                 cropf_arr,
                 self.whc_fp,
-                self.prec_ds,
-                self.temp_ds,
+                self.prec_fp,
+                self.temp_fp,
             ).res, self.res)
 
         # instantiating a simulation with rasters of mistmatching shapes
         # should raise a ValueError
         self.assertRaises(ValueError, pst.MonthlySimulation, dem_arr[1:, 1:],
-                          self.cropf_fp, self.whc_fp, self.prec_ds,
-                          self.temp_ds)
+                          self.cropf_fp, self.whc_fp, self.prec_fp,
+                          self.temp_fp)
         self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
-                          cropf_arr[1:, 1:], self.whc_fp, self.prec_ds,
-                          self.temp_ds)
+                          cropf_arr[1:, 1:], self.whc_fp, self.prec_fp,
+                          self.temp_fp)
         self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
-                          self.cropf_fp, whc_arr[1:, 1:], self.prec_ds,
-                          self.temp_ds)
+                          self.cropf_fp, whc_arr[1:, 1:], self.prec_fp,
+                          self.temp_fp)
 
         # test that in any case, the dem is an instance of richdem.rdarray
-        sts_from_arr = pst.MonthlySimulation(dem_arr, cropf_arr, whc_arr,
-                                             self.prec_ds, self.temp_ds,
-                                             res=self.res)
-        sts_from_fp = pst.MonthlySimulation(self.dem_fp, cropf_arr, whc_arr,
-                                            self.prec_ds, self.temp_ds)
-        self.assertIsInstance(sts_from_arr.dem, richdem.rdarray)
-        self.assertIsInstance(sts_from_fp.dem, richdem.rdarray)
+        ms_from_arr = pst.MonthlySimulation(dem_arr, cropf_arr, whc_arr,
+                                            self.prec_fp, self.temp_fp,
+                                            res=self.res)
+        ms_from_fp = pst.MonthlySimulation(self.dem_fp, cropf_arr, whc_arr,
+                                           self.prec_fp, self.temp_fp)
+        self.assertIsInstance(ms_from_arr.dem, richdem.rdarray)
+        self.assertIsInstance(ms_from_fp.dem, richdem.rdarray)
 
-        # finally, ensure that the 'state variables' (i.e., the instance
-        # attributes that keep track of the snow accumulated, available water,
-        # and ground water at each simulation step) match the shape of the
-        # rest of arrays
-        dem_from_arr_shape = sts_from_arr.dem.shape
-        dem_from_fp_shape = sts_from_fp.dem.shape
+        # ensure that the 'state variables' (i.e., the instance attributes
+        # that keep track of the snow accumulated, available water, and ground
+        # water at each simulation step) match the shape of the rest of arrays
+        dem_from_arr_shape = ms_from_arr.dem.shape
+        dem_from_fp_shape = ms_from_fp.dem.shape
         for state_variable in [
                 'snow_accum', 'available_water', 'ground_water'
         ]:
             self.assertEqual(
-                getattr(sts_from_arr, state_variable).shape,
-                dem_from_arr_shape)
+                getattr(ms_from_arr, state_variable).shape, dem_from_arr_shape)
 
             self.assertEqual(
-                getattr(sts_from_fp, state_variable).shape, dem_from_fp_shape)
+                getattr(ms_from_fp, state_variable).shape, dem_from_fp_shape)
+
+    def test_io_climatological(self):
+        # test that the varnames and number of months are extracted correctly
+        # from filepaths
+        ms = pst.MonthlySimulation(self.dem_fp, self.cropf_fp, self.whc_fp,
+                                   self.prec_fp, self.temp_fp)
+        self.assertEqual(ms.prec_varname, 'prec')
+        self.assertEqual(ms.temp_varname, 'temp')
+        self.assertEqual(ms.num_months, self.num_months)
+
+        # test that passing wrong varnames raises a ValueError
+        self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
+                          self.cropf_fp, self.whc_fp, self.prec_fp,
+                          self.temp_fp, prec_varname='foo')
+        self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
+                          self.cropf_fp, self.whc_fp, self.prec_fp,
+                          self.temp_fp, temp_varname='foo')
+
+        # now test messing with the datasets
+        prec_ds = xr.open_dataset(self.prec_fp, decode_times=False)
+        temp_ds = xr.open_dataset(self.temp_fp, decode_times=False)
+
+        # TODO: test that there must be a time dimension and (x, y)/(lon, lat)
+        # coordinates, and that they match the terrain data
+        # self.assertRaises(ValueError, pst.MonthlySimulation,
+        #                   self.dem_fp, self.cropf_fp, self.whc_fp,
+        #                   prec_ds.drop('time'), temp_ds)
+        # self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
+        #                   self.cropf_fp, self.whc_fp, prec_ds,
+        #                   temp_ds.drop('time'))
+
+        # test that time dimensions match
+        wrong_slice = slice(0, self.num_months - 1)
+        self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
+                          self.cropf_fp, self.whc_fp,
+                          prec_ds.isel(time=wrong_slice), temp_ds)
+        self.assertRaises(ValueError, pst.MonthlySimulation, self.dem_fp,
+                          self.cropf_fp, self.whc_fp, prec_ds,
+                          temp_ds.isel(time=wrong_slice))
